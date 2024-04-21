32cfd8db3a065ebdef8b84d45da087b13316ce56
Chan Myae Oo <chanmyaeoodev@gmail.com>
2024-04-21 21:39:07 +0630

    add loading

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("loading-page").style.display = "none";
+  }, 3000);
+});
+
 // Import our custom CSS
 import "../scss/styles.scss";
 
@@ -8,8 +14,3 @@ import "animate.css";
 // import ScrollReveal from "scrollreveal/dist/scrollreveal.min";
 import ScrollReveal from "scrollreveal";
 ScrollReveal({ reset: true }).reveal(".description_box", { delay: 250 });
-
-ScrollReveal({ reset: true }).reveal(".headline2");
-ScrollReveal({ reset: true }).reveal(".tagline", { delay: 500 });
-ScrollReveal({ reset: true }).reveal(".punchline", { delay: 2000 });
-ScrollReveal().reveal(".widget", { interval: 200 });
